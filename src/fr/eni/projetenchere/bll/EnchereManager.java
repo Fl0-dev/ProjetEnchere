70d18a516ce1cb6b3a7cb5240af5cfacf7713637
Fl0-dev <florian.girard.dev@outlook.fr>
2021-06-22 14:31:39 +0200

    Add EnchereManager getInstance() et appel à la DAOFactory dans bll.EnchereManager Add List<Enchere> selectAllEnchere() dans bll.EnchereManager Add List<Enchere> selectAllEnchere() dans dal.EnchereDAO Add List<Enchere> selectAllEnchere() dans dal.EnchereDAOJdbcImpl

--- src/fr/eni/projetenchere/bll/EnchereManager.java
+++ src/fr/eni/projetenchere/bll/EnchereManager.java
@@ -1,5 +1,76 @@
 package fr.eni.projetenchere.bll;
 
+import java.util.List;
+
+import fr.eni.projetenchere.bo.Enchere;
+import fr.eni.projetenchere.dal.DAOFactory;
+import fr.eni.projetenchere.dal.EnchereDAO;
+
+/**
+ * Classe de la BLL contenant les méthodes
+ * utilisées en Vue et utilisant les méthodes de la DAL
+ * @author Florian
+ * @version 1.0
+ * @date 22/06/21 
+ */
 public class EnchereManager {
 
+	//création d'instance
+	private static EnchereManager instance;
+	
+	//Singleton du manager
+	public static EnchereManager getInstance() {
+		if(instance == null) {
+			instance = new EnchereManager();
+		}
+		return instance;
+	}
+		
+	//
+	private EnchereDAO enchereDAO;
+	
+	//constructeur
+	private EnchereManager() {
+		enchereDAO = DAOFactory.getEnchereDAO();
+	}
+	
+	
+	/**
+	 * récupère toutes les enchères en cours dans
+	 * @return listeEncheres 
+	 * avec enchère(dateEnchere,montantEnchere,utilisateur(pseudo), articleVendu(nomArticle)) 
+	 */
+	public List<Enchere> selectAllEnchere(){
+		//création de la liste des enchères
+		List<Enchere> listeEncheres;
+		
+		listeEncheres = enchereDAO.selectAllEnchere();
+		
+		return listeEncheres;
+		//TODO gestion des exceptions
+	}
+	
+	/**
+	 * récupère toutes les enchères selon
+	 * @param nomArticle
+	 * @param noCategorie
+	 * @return listeEncheresByCategorie
+	 * avec enchère(dateEnchere,montantEnchere,utilisateur(pseudo), articleVendu(nomArticle)) 
+	 */
+//	public List<Enchere> selectEnchereByCategorie(String nomArticle,int noCategorie){
+//		List<Enchere> listeEncheresByCategorie;
+//		
+//		listeEncheresByCategorie =enchereDAO.selectEnchereByCategorie(nomArticle,noCategorie);
+//		
+//		return listeEncheresByCategorie;
+//	}
+//	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/fr/eni/projetenchere/dal/EnchereDAOJdbcImpl.java
+++ src/fr/eni/projetenchere/dal/EnchereDAOJdbcImpl.java
@@ -1,5 +1,72 @@
 package fr.eni.projetenchere.dal;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import fr.eni.projetenchere.bo.ArticleVendu;
+import fr.eni.projetenchere.bo.Enchere;
+import fr.eni.projetenchere.bo.Utilisateur;
+
+
 public class EnchereDAOJdbcImpl implements EnchereDAO {
 
+	/**
+	 * selectionne en DB toutes les enchères en cours et les met dans une liste
+	 * @return listeEncheres
+	 */
+	@Override
+	public List<Enchere> selectAllEnchere(){
+		//création de la liste vide
+		List<Enchere> listeEncheres = new ArrayList<>();
+		
+		//requête SQL
+		final String SELECT_ALL_ENCHERE = "select date_enchere, montant_enchere, a.nom_article, u.pseudo from ENCHERES as e inner join UTILISATEURS as u on e.no_utilisateur = u.no_utilisateur inner join ARTICLES_VENDUS as a on a.no_article= e.no_article;";
+		
+		//ouverture de la connexion vers DB
+		try (Connection connection = JdbcTools.getConnection();
+	             Statement requete = connection.createStatement()) {
+			//récupération du résultat
+			ResultSet rs = requete.executeQuery(SELECT_ALL_ENCHERE);
+			
+			//création des variables
+			ArticleVendu articleVendu;
+			Utilisateur utilisateur;
+			Enchere enchere;
+			
+			//récupération en Java
+			while (rs.next()) {
+				LocalDate dateEnchere = rs.getDate("date_enchere").toLocalDate();
+				int montantEnchere = rs.getInt("montant_enchere");
+				String nomArticle = rs.getString("a.nom_article");
+				String pseudo = rs.getString("u.pseudo");
+				
+				//utilisation des résultats
+				utilisateur = new Utilisateur(pseudo);
+				articleVendu = new ArticleVendu(nomArticle);
+				enchere = new Enchere(dateEnchere,montantEnchere,utilisateur,articleVendu);
+				
+				//ajout dans la liste d'enchères
+				listeEncheres.add(enchere);
+			}
+			
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		return listeEncheres;
+	}
+	
+	/**
+	 * selectionne en DB toutes les enchères en cours selon
+	 * @param nomArticle
+	 * @param noCategorie
+	 * @return listeEncheres
+	 */
+	//List<Enchere> selectEnchereByCategorie(String nomArticle, int noCategorie){
+		
+	//}
+	
 }
