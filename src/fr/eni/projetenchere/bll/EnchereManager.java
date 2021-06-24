49152b913b7e519f7f19e2109ed25a7076394bc3
Fl0-dev <florian.girard.dev@outlook.fr>
2021-06-24 14:56:35 +0200

    fix methode insertUtilisateur

--- src/fr/eni/projetenchere/bll/EnchereManager.java
+++ src/fr/eni/projetenchere/bll/EnchereManager.java
@@ -8,104 +8,113 @@ import fr.eni.projetenchere.dal.DAOFactory;
 import fr.eni.projetenchere.dal.EnchereDAO;
 
 /**
- * Classe de la BLL contenant les méthodes
- * utilisées en Vue et utilisant les méthodes de la DAL
+ * Classe de la BLL contenant les méthodes utilisées en Vue et utilisant les
+ * méthodes de la DAL
+ * 
  * @author Florian
  * @version 1.0
- * @date 22/06/21 
+ * @date 22/06/21
  */
 public class EnchereManager {
 
-	//création d'instance
+	// création d'instance
 	private static EnchereManager instance;
-	
-	//Singleton du manager
+
+	// Singleton du manager
 	public static EnchereManager getInstance() {
-		if(instance == null) {
+		if (instance == null) {
 			instance = new EnchereManager();
 		}
 		return instance;
 	}
-		
+
 	//
 	private EnchereDAO enchereDAO;
-	
-	//constructeur
+
+	// constructeur
 	private EnchereManager() {
 		enchereDAO = DAOFactory.getEnchereDAO();
 	}
-	
+
 	/**
 	 * récupère toutes les enchères en cours dans
-	 * @return listeEncheres 
-	 * avec enchère(dateEnchere,montantEnchere,utilisateur(pseudo), articleVendu(nomArticle)) 
+	 * 
+	 * @return listeEncheres avec
+	 *         enchère(dateEnchere,montantEnchere,utilisateur(pseudo),
+	 *         articleVendu(nomArticle))
 	 */
-	public List<Enchere> selectAllEnchere(){
-		//création de la liste des enchères
+	public List<Enchere> selectAllEnchere() {
+		// création de la liste des enchères
 		List<Enchere> listeEncheres;
 		listeEncheres = enchereDAO.selectAllEnchere();
 		return listeEncheres;
-		//TODO gestion des exceptions
+		// TODO gestion des exceptions
 	}
 
-
 	/**
-	 * récupère email, pseudo et mot de passe des utilisateurs pour l'accès à la connexion
+	 * récupère email, pseudo et mot de passe des utilisateurs pour l'accès à la
+	 * connexion
+	 * 
 	 * @return listeUtilisateurConnexion
-	 *  
+	 * 
 	 */
 	public List<Utilisateur> selectConnexion() {
-		//création de la liste des utilisateurs pour la connexion
+		// création de la liste des utilisateurs pour la connexion
 		List<Utilisateur> listeUtilisateurConnexion;
-		
+
 		listeUtilisateurConnexion = enchereDAO.selectConnexion();
 		return listeUtilisateurConnexion;
-		//TODO gestion des exceptions
+		// TODO gestion des exceptions
 	}
-	
+
 	/**
 	 * récupère toutes les enchères selon
+	 * 
 	 * @param nomArticle
 	 * @param noCategorie
-	 * @return listeEncheresByCategorie
-	 * avec enchère(dateEnchere,montantEnchere,utilisateur(pseudo), articleVendu(nomArticle)) 
+	 * @return listeEncheresByCategorie avec
+	 *         enchère(dateEnchere,montantEnchere,utilisateur(pseudo),
+	 *         articleVendu(nomArticle))
 	 */
-	public List<Enchere> selectEnchereByCatAndArt(String nomArticle,int noCategorie){
+	public List<Enchere> selectEnchereByCatAndArt(String nomArticle, int noCategorie) {
 		List<Enchere> listeEncheresByCategorie;
-		
-		listeEncheresByCategorie =enchereDAO.selectEnchereByCatAndArt(nomArticle,noCategorie);
-		
+
+		listeEncheresByCategorie = enchereDAO.selectEnchereByCatAndArt(nomArticle, noCategorie);
+
 		return listeEncheresByCategorie;
 	}
-	
+
 	/**
 	 * récupère toutes les enchères selon
+	 * 
 	 * @param nomArticle
 	 * @return listeEncheresByArticle
 	 */
-	public List<Enchere> selectEnchereByArticle(String nomArticle){
+	public List<Enchere> selectEnchereByArticle(String nomArticle) {
 		List<Enchere> listeEncheresByArticle;
-		
-		listeEncheresByArticle =enchereDAO.selectEnchereByArticle(nomArticle);
-		
+
+		listeEncheresByArticle = enchereDAO.selectEnchereByArticle(nomArticle);
+
 		return listeEncheresByArticle;
 	}
+
 	/**
 	 * récupère les catégories et
-	 * @return listeCategories
-	 * à utiliser pour les options du menu déroulant
+	 * 
+	 * @return listeCategories à utiliser pour les options du menu déroulant
 	 */
-	public List<Categorie> selectCategorie(){
-		//création de la liste des enchères
+	public List<Categorie> selectCategorie() {
+		// création de la liste des enchères
 		List<Categorie> listeCategories;
 		listeCategories = enchereDAO.selectCategorie();
 		return listeCategories;
-		//TODO gestion exception
-		
+		// TODO gestion exception
+
 	}
-	
+
 	/**
 	 * ajoute un utilisateur après l'avoir créer avec les données fournies :
+	 * 
 	 * @param pseudo
 	 * @param nom
 	 * @param prenom
@@ -115,56 +124,59 @@ public class EnchereManager {
 	 * @param codePostal
 	 * @param ville
 	 * @param motDePasse
-	 * @param confirmation 
+	 * @param confirmation
 	 * @return newUtilisateur
 	 */
-	public Utilisateur inserNewtUtilisateur(String pseudo, String nom, String prenom, String email, String telephone , String rue,
-			String codePostal, String ville, String motDePasse){
-		Utilisateur newUtilisateur= new Utilisateur(pseudo, nom, prenom, email, telephone,rue, codePostal, ville, motDePasse);
-		enchereDAO.insertUtilisateur(newUtilisateur); 
-		
-			return newUtilisateur;
+	public Utilisateur inserNewtUtilisateur(String pseudo, String nom, String prenom, String email, String telephone,
+			String rue, String codePostal, String ville, String motDePasse) {
+		Utilisateur newUtilisateur= new Utilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville,
+				motDePasse);
+		enchereDAO.insertUtilisateur(newUtilisateur);
+
+		return newUtilisateur;
 	}
-	
+
 	/**
-	 * vérifie si l'email est en DB
+	 *  si l'email est en DB
 	 * @param email
 	 * @throws Exception
 	 */
-	public void validationEmail(String email) throws Exception{
-		//appel du manager pour récupérer une liste de tous les emails en DB
+	public void validationEmail(String email) throws Exception {
+		// appel du manager pour récupérer une liste de tous les emails en DB
 		List<Utilisateur> listPseudoEmail = EnchereManager.getInstance().selectConnexion();
-		//si l'email n'est pas null et pas vide
-		if ( email != null && email.trim().length() != 0 ) {
-			//compare dans la DB
+		// si l'email n'est pas null et pas vide
+		if (email != null && email.trim().length() != 0) {
+			// compare dans la DB
 			for (Utilisateur utilisateur : listPseudoEmail) {
-				if(email.equals(utilisateur.getEmail())) {
-					throw new Exception("L'email existe déjà"); 	
+				if (email.equals(utilisateur.getEmail())) {
+					throw new Exception("L'email existe déjà");
 				}
 				break;
 			}
 		}
 	}
+
 	/**
-	 * permet de vérifier si le pseudo est valide 
+	 * permet de vérifier si le pseudo est valide
+	 * 
 	 * @param pseudo
 	 * @throws Exception
 	 */
-	public void validationPseudo(String pseudo) throws Exception{
-		//appel du manager pour récupérer une liste de tous les pseudo en DB
+	public void validationPseudo(String pseudo) throws Exception {
+		// appel du manager pour récupérer une liste de tous les pseudo en DB
 		List<Utilisateur> listPseudoEmail = EnchereManager.getInstance().selectConnexion();
-		//si le pseudo n'est pas null et pas vide
+		// si le pseudo n'est pas null et pas vide
 		if (pseudo != null && pseudo.trim().length() != 0) {
-			//si il ne contient que des caractères alphanumériques
+			// si il ne contient que des caractères alphanumériques
 			if (!pseudo.matches("#^[:alnum:]$#")) {
 				throw new Exception("Le pseudo ne doit contenir que des caractères alphanumériques");
 			}
-		} 
-		
-		//compare le pseudo aux pseudos de la database
+		}
+
+		// compare le pseudo aux pseudos de la database
 		for (Utilisateur utilisateur : listPseudoEmail) {
 			if (pseudo.equals(utilisateur.getPseudo())) {
-				throw new Exception("Le pseudo existe déjà"); 	
+				throw new Exception("Le pseudo existe déjà");
 			}
 			break;
 		}
@@ -172,17 +184,31 @@ public class EnchereManager {
 
 	/**
 	 * Valide le mot de passe
+	 * 
 	 * @param motDePasse
 	 * @param confirmation
 	 * @throws Exception
 	 */
-	public void validationMotsDePasse( String motDePasse, String confirmation ) throws Exception{
-	    if (motDePasse != null && motDePasse.trim().length() != 0 && confirmation != null && confirmation.trim().length() != 0) {
-	        if (!motDePasse.equals(confirmation)) {
-	            throw new Exception("Les mots de passe entrés sont différents, merci de les saisir à nouveau.");
-	        } 
-	    } else {
-	        throw new Exception("Merci de saisir et confirmer votre mot de passe.");
-	    }
+	public void validationMotsDePasse(String motDePasse, String confirmation) throws Exception {
+		if (motDePasse != null && motDePasse.trim().length() != 0 && confirmation != null
+				&& confirmation.trim().length() != 0) {
+			if (!motDePasse.equals(confirmation)) {
+				throw new Exception("Les mots de passe entrés sont différents, merci de les saisir à nouveau.");
+			}
+		} else {
+			throw new Exception("Merci de saisir et confirmer votre mot de passe.");
+		}
 	}
+
+	
+	public Utilisateur selectUtilisateurByPseudo(String pseudo) {
+		
+		Utilisateur utilisateur;
+		
+		utilisateur = enchereDAO.selectUtilisateurByPseudo(pseudo);
+		
+		return utilisateur;
+		//TODO : Gestion exception
+	}
+
 }
