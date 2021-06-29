dce429214e7a1c39c0e57422138f53f3140b0287
Shun Thomar <xunthom@gmail.com>
2021-06-29 22:03:59 +0200

    Fix requête selectarticlebyid

--- src/fr/eni/projetenchere/servlets/ServletEncherir.java
+++ src/fr/eni/projetenchere/servlets/ServletEncherir.java
@@ -8,9 +8,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import fr.eni.projetenchere.bll.EnchereManager;
 import fr.eni.projetenchere.bo.ArticleVendu;
+import fr.eni.projetenchere.bo.Utilisateur;
 
 @WebServlet("/ServletEncherir")
 public class ServletEncherir extends HttpServlet {
@@ -24,18 +26,26 @@ public class ServletEncherir extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/*
-		 * récupération du numéro d'article
-		 */
-		//int no_article = Integer.parseInt(request.getParameter("no_article"));
+		// récupère l'utilisateur de la session
+		HttpSession session = request.getSession();
+		Utilisateur utilisateurSession = (Utilisateur) session.getAttribute("utilisateurSession");
 		
-		int no_article = 1;
-		ArticleVendu articleSelected = EnchereManager.getInstance().selectArticleById(no_article);
-
-		request.setAttribute("articleSelected", articleSelected);
+		if (utilisateurSession != null) {
+			//int no_article = Integer.parseInt(request.getParameter("no_article"));
+			// récupère id article pour l'afficher
+			int no_article = 1;
+			ArticleVendu articleSelected = EnchereManager.getInstance().selectArticleById(no_article);
+			request.setAttribute("articleSelected", articleSelected);
+			System.out.println(articleSelected);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/Encherir.jsp");
+			rd.forward(request, response);
+		} else {
+			// renvoie vers l'accueil
+			RequestDispatcher rd = request.getRequestDispatcher("/Accueil");
+			rd.forward(request, response);
+		}
 		
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/Encherir.jsp");
-		rd.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
